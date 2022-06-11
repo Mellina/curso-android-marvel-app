f754dc53386b9b1cc82fd1a6a5b30f79dd6776af
mellina.c.cerqueira <CasaDaArte86>
2022-06-11 19:52:37 -0300

    configuracao para testes de ui

---
+++ app/src/debug/java/com/example/marvelapp/HiltTestActivity.kt
@@ -0,0 +1,10 @@
+package com.example.marvelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity() {
+
+}
